b482fcc6c45a9f1c20b81cc5d12bb6232ab56837
Pseudomonas <zehm0007@d.umn.edu>
2016-09-17 19:00:34 -0500

    Update hello.c
    remove some dumb old comments

--- hello.c
+++ hello.c
@@ -9,6 +9,3 @@ int main()
   return 0;
 }
 //Will compile with gcc, use g++ for C++
-//Let myself know if it works in sudo
-//Maybe running random bits of code requires super user
-//makes sense, good security 
